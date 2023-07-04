5478c5ec4438daf47cf67f036b798d9ea322b746
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2023-07-04 16:21:47 +0200

    add manifest feature to tests

--- features/mod.ts
+++ features/mod.ts
@@ -1,4 +1,5 @@
 export * as litDoc from "./lit-doc/mod.ts";
+export * as manifest from "./manifest/mod.ts";
 export * as markdown from "./markdown/mod.ts";
 export * as monaco from "./monaco/mod.ts";
 export * as text from "./text/mod.ts";
