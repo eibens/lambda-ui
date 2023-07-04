6e6096f0ac689391b22afea4f50ccd8580ad7aa1
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2023-07-04 18:29:00 +0200

    fix button stroke and disable theme toggle outside browser

--- features/client/use_env.ts
+++ features/client/use_env.ts
@@ -2,13 +2,15 @@ import { useEffect, useState } from "preact/hooks";
 
 export function useEnv() {
   const [env, setEnv] = useState({
-    isBrowser: false,
+    isFirstRedraw: false,
+    isBrowser: typeof globalThis.document !== "undefined",
   });
 
   useEffect(() => {
     requestAnimationFrame(() => {
       setEnv({
         isBrowser: true,
+        isFirstRedraw: true,
       });
     });
   }, []);
--- features/widgets/theme_toggle.tsx
+++ features/widgets/theme_toggle.tsx
@@ -1,4 +1,5 @@
 import { DarkMode } from "icons/dark_mode.tsx";
+import { useEnv } from "../client/mod.ts";
 import { useTheme } from "../theme/use_theme.ts";
 import { Button, ButtonProps } from "../widgets/button.tsx";
 
@@ -6,12 +7,14 @@ export function ThemeToggle(props: {
   size?: ButtonProps["size"];
 }) {
   const theme = useTheme();
+  const { isBrowser } = useEnv();
   return (
     <Button
       tag="button"
       onClick={() => theme.toggle()}
       icon={<DarkMode />}
       iconPosition="right"
+      disabled={!isBrowser}
       size={props.size}
     />
   );
