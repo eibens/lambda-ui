90db3ffa4fe867ee1373ee0539c83f8abbe0361f
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2023-07-17 02:18:15 +0200

    cleanup obsolete files

--- features/lit-doc2/cli.ts
+++
@@ -1,5 +0,0 @@
-import { cli } from "./packages/cli/mod.ts";
-
-if (import.meta.main) {
-  await cli();
-}
--- features/lit-doc2/packages/cli/mod.ts
+++
@@ -1,5 +0,0 @@
-export async function cli() {
-  console.log("Welcome to the lit-doc CLI.");
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("Bye!");
-}
