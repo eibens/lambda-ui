e66a9a09fcb28f142645928ff83f3a7605b3b988
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2023-09-09 17:13:57 +0200

    upgrade fresh

--- dev.ts
+++ dev.ts
@@ -1,6 +1,9 @@
-#!/usr/bin/env -S deno run -A --watch=static/,routes/,features/
-import fresh from "$fresh/dev.ts";
+#!/usr/bin/env -S deno run -A --watch=static/,routes/
+
+import dev from "$fresh/dev.ts";
+import "$std/dotenv/load.ts";
 import litdoc from "litdoc/dev/mod.ts";
+import config from "./fresh.config.ts";
 
 await litdoc(import.meta.url, {
   routes: {
@@ -15,4 +18,4 @@ await litdoc(import.meta.url, {
   },
 });
 
-await fresh(import.meta.url, "./main.ts");
+await dev(import.meta.url, "./main.ts", config);
